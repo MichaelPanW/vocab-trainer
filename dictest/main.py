9e79b92ab32059a28a6a147d4e9fbd5b980b383e
潘嶧德 <m28126584@gmail.com>
2025-04-12 14:07:30 +0800

    更新 config.py 以支援中文提示，並修改 main.py 以簡化題庫選擇流程及錯誤處理。

--- dictest/main.py
+++ dictest/main.py
@@ -3,31 +3,43 @@
 import random
 import json
 import config
+import os
 
 __author__ = "dsm_helper"
-
+data_path = "../data"
 
 def get_quiz_data_from_input():
-    return input("입학년도를 입력해주세요 ex) 2017 >> "), input("학기를 입력해주세요 ex) 1st >> "), input("파일명을 입력해주세요 ex) 01 >> ")
+    return input("請輸入要測驗的題庫(0) >> ")
 
 
-def get_words_by_info(enter_year, semester, file_name):
+def get_words_by_info(file_name):
     try:
-        with open(f'../data/dsm{enter_year}/{semester}/{file_name}.json', 'r', encoding="UTF8") as f:
+        with open(f'{data_path}/{file_name}', 'r', encoding="UTF8") as f:
             words = json.load(f)
         return words
     except Exception as e:
-        print("잘못된 입력입니다. 다시 확인해주세요.")
+        print("輸入錯誤，請重新確認。")
         print(e)
         exit(1)
 
-if __name__ == "__main__":
+def get_data_list():
+    # 只取json檔案
+    data_list = [f for f in os.listdir(data_path) if f.endswith('.json')]
+    return data_list
 
-    enter_year, semester, file_name = get_quiz_data_from_input()
+def show_data_list(data_list):
+    print("選擇要測驗的題庫")
+    for i, filename in enumerate(data_list):
+        print(f"{i}: {filename}")
+
+if __name__ == "__main__":
+    data_list = get_data_list()
+    show_data_list(data_list)
+    index = int(input("請輸入要測驗的題庫編號 >> "))
 
     check = True
     count = 0
-    words = get_words_by_info(enter_year, semester, file_name)
+    words = get_words_by_info(data_list[index])
 
     def finish_quiz():
         print(config.EXIT_MESSAGE)
@@ -41,8 +53,10 @@ if __name__ == "__main__":
             answer, meaning = random.choice(list(words.items()))
             check = False
             print(meaning)
+            # 顯示頭尾
+            print(answer[0],'_'*len(answer), answer[-1])
 
-        question = input("I think.. >> ")
+        question = input("Ans >> ")
 
         if question == answer:
             check = True
